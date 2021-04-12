aa8515ac0f3cfef44bfc3148d92143a686289b7f
diyigemt <qwe1355247243@gmail.com>
2021-04-12 16:19:08 +0800

    test cls

--- src/main/java/com/minesweeper/Main.java
+++ src/main/java/com/minesweeper/Main.java
@@ -1,8 +1,34 @@
 package com.minesweeper;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    boolean running = true;
+    Scanner scanner = new Scanner(System.in);
+    while (running) {
+      System.out.println("Hello");
+      int select = scanner.nextInt();
+      if (select == 1) {
+        running = false;
+      }
+      Runtime runtime = Runtime.getRuntime();
+      try {
+        String s = System.getProperty("os.name").toLowerCase();
+        if (s.contains("linux")) {
+          runtime.exec("clear");
+        } else {
+//          System.out.print("\033[H\033[2J");
+//          System.out.flush();
+//          runtime.exec("cls");
+          ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+          pb.inheritIO().start().waitFor();
+        }
+      } catch (IOException | InterruptedException e) {
+        e.printStackTrace();
+      }
     }
+  }
 }
--- src/test/java/TestSimple.java
+++ src/test/java/TestSimple.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class TestSimple {
   @Test
   public void testMainPage() {
-    
+
   }
 }
