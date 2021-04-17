bbda8e0e9e3ab15bfe60107574ba4baec7b2a777
duo <814891011@qq.com>
2021-04-17 20:51:44 +0800

    Update LayBombToMap.java
    add more annotation

--- src/main/java/com/minesweeper/map/LayBombToMap.java
+++ src/main/java/com/minesweeper/map/LayBombToMap.java
@@ -11,9 +11,10 @@ public class LayBombToMap {
 	static int layBombCount = 0;
 	
 	/**布置雷的函数，在游戏开始后第一次点击方块时call
-	 * @param map
-	 * @param row
-	 * @param col
+	 * @param map 使用的地图，为方块数组
+	 * @param row 第一次点击的方块对应的行数
+	 * @param col 第一次点击的方块对应的列数
+	 * @return BombCount为布置雷的个数，可用于测试layBomb()函数布置雷的个数
 	 */
 	public static int layBomb(Grid[][] map,int row,int col) {
 		int BombCount=0;
@@ -35,13 +36,14 @@ public class LayBombToMap {
 	}
 	/**
 	 * 计算每个方块周围雷数，写入方块中
-	 * @param map
+	 * @param map 使用的地图，为方块数组
 	 */
 	public static void computeCountAround(Grid[][] map) {
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
 				if (map[i][j].isMineTag() == false) {
 					int count = 0;
+					//计算周围3x3区域是否有雷，边界和顶点不为3x3，适当调整
 					for (int x = Math.max(0, i - 1); x <= Math.min(
 							Gate.sumRow - 1, i + 1); x++) {
 						for (int y = Math.max(0, j - 1); y <= Math.min(
